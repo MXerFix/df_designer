1dc1b97bb3c075a1422ab1b59ff26ddbdc594df4
MXerFix <maksim2003003@gmail.com>
2023-10-24 12:58:23 +0300

    update 0.0.37a

--- src/frontend/src/pages/FlowPage/index.tsx
+++ src/frontend/src/pages/FlowPage/index.tsx
@@ -21,7 +21,7 @@ export default function FlowPage() {
   }, [id]);
 
   // Initialize state variable for the version
-  const [version, setVersion] = useState("0.0.36a");
+  const [version, setVersion] = useState("0.0.37a");
   // useEffect(() => {
   //   getVersion().then((data) => {
   //     setVersion(data.version);
